1f8980af11f1d8700c74e4b611a40a1ba7502756
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-02-11 21:35:08 +0100

    v0.3.5

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='SimpleImageIO',
-    version='0.3.4',
+    version='0.3.5',
     author='Pascal Grittmann',
     url='https://github.com/pgrit/SimpleImageIO',
 
