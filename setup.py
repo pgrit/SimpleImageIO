a95ea2981f07d21f223addc91f38961d92fdce5e
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-07-22 20:51:40 +0200

    fix missing file in python package

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='SimpleImageIO',
-    version='0.7.4',
+    version='0.7.5',
     author='Pascal Grittmann',
     url='https://github.com/pgrit/SimpleImageIO',
 
