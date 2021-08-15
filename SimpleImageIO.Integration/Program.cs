fd513bcc6cfd74a78594f498e7cd3f8f89f19191
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-08-15 12:14:17 +0200

    remove debug code

--- SimpleImageIO.Integration/Program.cs
+++ SimpleImageIO.Integration/Program.cs
@@ -1,23 +1,14 @@
 ﻿namespace SimpleImageIO.Integration {
     class Program {
         static int Main(string[] args) {
-             RgbImage image = new(10, 15);
-            for (int row = 0; row < 15; ++row)
-                for (int col = 0; col < 10; ++col)
-                    image.SetPixel(col, row, new(row/15.0f));
+            TestDenoise.TestPathTracer();
+            TestDenoise.TestBidir();
+            TestDenoise.TestVcm();
 
-            image.WriteToFile("written-direct" + ".exr");
-            byte[] generated = image.WriteToMemory(".exr");
-            System.IO.File.WriteAllBytes("written-mem.exr", generated);
+            TestTonemap.Reinhard();
+            TestTonemap.ACES();
 
-            // TestDenoise.TestPathTracer();
-            // TestDenoise.TestBidir();
-            // TestDenoise.TestVcm();
-
-            // TestTonemap.Reinhard();
-            // TestTonemap.ACES();
-
-            // ValidateTevIpc.TestTev();
+            ValidateTevIpc.TestTev();
 
             return 0;
         }
