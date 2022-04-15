b35e36c35303cbc6263aeb786ec26df6dbe3ee18
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2022-04-15 16:22:58 +0200

    faster histograms

--- SimpleImageIO/Histogram.cs
+++ SimpleImageIO/Histogram.cs
@@ -1,87 +1,92 @@
-namespace SimpleImageIO {
+namespace SimpleImageIO;
+
+/// <summary>
+/// Computes and stores a histogram of pixel channel values in an image.
+/// </summary>
+public class Histogram {
+    int[] counts;
+    int numPixels;
+
     /// <summary>
-    /// Computes and stores a histogram of pixel channel values in an image.
+    /// Smallest value in the image
     /// </summary>
-    public class Histogram {
-        int[] counts;
-        int numPixels;
+    public float Min { get; private set; }
 
-        /// <summary>
-        /// Smallest value in the image
-        /// </summary>
-        public float Min { get; private set; }
+    /// <summary>
+    /// Larges value in the image
+    /// </summary>
+    public float Max { get; private set; }
 
-        /// <summary>
-        /// Larges value in the image
-        /// </summary>
-        public float Max { get; private set; }
+    /// <summary>
+    /// Average of all pixel values
+    /// </summary>
+    public float Average { get; private set; }
 
-        /// <summary>
-        /// Average of all pixel values
-        /// </summary>
-        public float Average { get; private set; }
+    /// <summary>
+    /// Number of bins in the histogram
+    /// </summary>
+    public int Resolution { get; private set; }
 
-        /// <summary>
-        /// Number of bins in the histogram
-        /// </summary>
-        public int Resolution { get; private set; }
+    /// <summary>
+    /// The value of the ith bin in the histogram. Indices outside the actual range are mapped to zero.
+    /// </summary>
+    /// <param name="idx">0-based index of the histogram bin, 0 is darkest value</param>
+    /// <returns>Number of pixels within the bin and the value in the bin's center</returns>
+    public (int Count, float Center) this[int idx]
+    => (counts[System.Math.Clamp(idx, 0, Resolution - 1)], (Max - Min) / Resolution * (idx + 0.5f) + Min);
 
-        /// <summary>
-        /// The value of the ith bin in the histogram. Indices outside the actual range are mapped to zero.
-        /// </summary>
-        /// <param name="idx">0-based index of the histogram bin, 0 is darkest value</param>
-        /// <returns>Number of pixels within the bin and the value in the bin's center</returns>
-        public (int Count, float Center) this[int idx]
-        => (counts[System.Math.Clamp(idx, 0, Resolution - 1)], (Max - Min) / Resolution * (idx + 0.5f) + Min);
+    /// <summary>
+    /// Initializes a histogram
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="resolution">Number of bins in the histogram</param>
+    /// <param name="channel">Index of the color channel</param>
+    public Histogram(ImageBase image, int resolution = 100, int channel = 0) {
+        Resolution = resolution;
 
-        /// <summary>
-        /// Initializes a histogram
-        /// </summary>
-        /// <param name="image">The image</param>
-        /// <param name="resolution">Number of bins in the histogram</param>
-        /// <param name="channel">Index of the color channel</param>
-        public Histogram(ImageBase image, int resolution = 100, int channel = 0) {
-            Resolution = resolution;
+        counts = new int[resolution];
+        numPixels = image.Width * image.Height;
 
-            counts = new int[resolution];
-            numPixels = image.Width * image.Height;
-
-            // Find minimum and maximum values
-            Min = float.MaxValue;
-            Max = float.MinValue;
-            Average = 0.0f;
-            for (int row = 0; row < image.Height; ++row) {
+        // Find minimum, maximum, and average values in a parallel-reduce scheme
+        Parallel.For<(float, float, float)>(0, image.Height, () => (float.MaxValue, float.MinValue, 0f),
+            (row, _, state) => {
                 for (int col = 0; col < image.Width; ++col) {
                     float c = image.GetPixelChannel(col, row, channel);
-                    Min = System.Math.Min(Min, c);
-                    Max = System.Math.Max(Max, c);
-                    Average += c;
+                    state.Item1 = System.Math.Min(state.Item1, c);
+                    state.Item2 = System.Math.Max(state.Item2, c);
+                    state.Item3 += c;
                 }
-            }
-            Average /= numPixels;
-
-            // Sort all pixels into the respective bins
-            for (int row = 0; row < image.Height; ++row) {
-                for (int col = 0; col < image.Width; ++col) {
-                    float c = image.GetPixelChannel(col, row, channel);
-                    float rel = (c - Min) / (Max - Min);
-                    int i = (int)(rel * resolution);
-                    i = System.Math.Clamp(i, 0, resolution - 1);
-                    counts[i]++;
+                return state;
+            }, (state) => {
+                lock (this) {
+                    Min = System.Math.Min(Min, state.Item1);
+                    Max = System.Math.Max(Max, state.Item2);
+                    Average += state.Item3;
                 }
-            }
-        }
+            });
 
-        /// <summary>
-        /// Computes the p-quantile.
-        /// </summary>
-        /// <param name="p">Ratio in [0, 1]</param>
-        /// <returns>Value greater than the portion p of all pixels</returns>
-        public float Quantile(float p) {
-            int num = (int)(p * numPixels);
-            int i = 0;
-            for (; num > 0 && i < Resolution; ++i) num -= counts[i];
-            return this[i-1].Center;
-        }
+        // Sort all pixels into the respective bins.
+        Parallel.For(0, image.Height, row => {
+            int localRow = row;
+            for (int col = 0; col < image.Width; ++col) {
+                float c = image.GetPixelChannel(col, localRow, channel);
+                float rel = (c - Min) / (Max - Min);
+                int i = (int)(rel * resolution);
+                i = System.Math.Clamp(i, 0, resolution - 1);
+                Interlocked.Increment(ref counts[i]);
+            }
+        });
+    }
+
+    /// <summary>
+    /// Computes the p-quantile.
+    /// </summary>
+    /// <param name="p">Ratio in [0, 1]</param>
+    /// <returns>Value greater than the portion p of all pixels</returns>
+    public float Quantile(float p) {
+        int num = (int)(p * numPixels);
+        int i = 0;
+        for (; num > 0 && i < Resolution; ++i) num -= counts[i];
+        return this[i - 1].Center;
     }
 }
--- SimpleImageIO/Metrics.cs
+++ SimpleImageIO/Metrics.cs
@@ -1,69 +1,67 @@
-using System.Diagnostics;
+namespace SimpleImageIO;
 
-namespace SimpleImageIO {
+/// <summary>
+/// Defines useful (error) metrics to compare and analyze images
+/// </summary>
+public static class Metrics {
     /// <summary>
-    /// Defines useful (error) metrics to compare and analyze images
+    /// Computes the mean square error of two images
     /// </summary>
-    public static class Metrics {
-        /// <summary>
-        /// Computes the mean square error of two images
-        /// </summary>
-        /// <param name="image">The first image</param>
-        /// <param name="reference">The second image</param>
-        public static float MSE(ImageBase image, ImageBase reference) {
-            Debug.Assert(image.Width == reference.Width);
-            Debug.Assert(image.Height == reference.Height);
-            Debug.Assert(image.NumChannels == reference.NumChannels);
-            return SimpleImageIOCore.ComputeMSE(image.DataPointer, image.NumChannels * image.Width,
-                reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
-                image.NumChannels);
-        }
+    /// <param name="image">The first image</param>
+    /// <param name="reference">The second image</param>
+    public static float MSE(ImageBase image, ImageBase reference) {
+        Debug.Assert(image.Width == reference.Width);
+        Debug.Assert(image.Height == reference.Height);
+        Debug.Assert(image.NumChannels == reference.NumChannels);
+        return SimpleImageIOCore.ComputeMSE(image.DataPointer, image.NumChannels * image.Width,
+            reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
+            image.NumChannels);
+    }
 
-        /// <summary>
-        /// Computes the relative mean square error of two images.
-        /// </summary>
-        /// <param name="image">The first image</param>
-        /// <param name="reference">The second image</param>
-        /// <returns>Mean of: Square error of each pixel, divided by the squared mean</returns>
-        public static float RelMSE(ImageBase image, ImageBase reference) {
-            Debug.Assert(image.Width == reference.Width);
-            Debug.Assert(image.Height == reference.Height);
-            Debug.Assert(image.NumChannels == reference.NumChannels);
-            return SimpleImageIOCore.ComputeRelMSE(image.DataPointer, image.NumChannels * image.Width,
-                reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
-                image.NumChannels);
-        }
+    /// <summary>
+    /// Computes the relative mean square error of two images.
+    /// </summary>
+    /// <param name="image">The first image</param>
+    /// <param name="reference">The second image</param>
+    /// <returns>Mean of: Square error of each pixel, divided by the squared mean</returns>
+    public static float RelMSE(ImageBase image, ImageBase reference) {
+        Debug.Assert(image.Width == reference.Width);
+        Debug.Assert(image.Height == reference.Height);
+        Debug.Assert(image.NumChannels == reference.NumChannels);
+        return SimpleImageIOCore.ComputeRelMSE(image.DataPointer, image.NumChannels * image.Width,
+            reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
+            image.NumChannels);
+    }
 
-        /// <summary>
-        /// Computes the relative mean square error of two images. Ignores a small percentage of the
-        /// brightest pixels. The result is less obscured by outliers this way.
-        /// </summary>
-        /// <param name="image">The first image</param>
-        /// <param name="reference">The second image</param>
-        /// <param name="percentage">Percentage of pixels to ignore</param>
-        public static float RelMSE_OutlierRejection(ImageBase image, ImageBase reference, float percentage = 0.1f) {
-            Debug.Assert(image.Width == reference.Width);
-            Debug.Assert(image.Height == reference.Height);
-            Debug.Assert(image.NumChannels == reference.NumChannels);
-            return SimpleImageIOCore.ComputeRelMSEOutlierReject(image.DataPointer, image.NumChannels * image.Width,
-                reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
-                image.NumChannels, percentage);
-        }
+    /// <summary>
+    /// Computes the relative mean square error of two images. Ignores a small percentage of the
+    /// brightest pixels. The result is less obscured by outliers this way.
+    /// </summary>
+    /// <param name="image">The first image</param>
+    /// <param name="reference">The second image</param>
+    /// <param name="percentage">Percentage of pixels to ignore</param>
+    public static float RelMSE_OutlierRejection(ImageBase image, ImageBase reference, float percentage = 0.1f) {
+        Debug.Assert(image.Width == reference.Width);
+        Debug.Assert(image.Height == reference.Height);
+        Debug.Assert(image.NumChannels == reference.NumChannels);
+        return SimpleImageIOCore.ComputeRelMSEOutlierReject(image.DataPointer, image.NumChannels * image.Width,
+            reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
+            image.NumChannels, percentage);
+    }
 
-        /// <summary>
-        /// Computes the relative mean square error of two images. Ignores a small percentage of the
-        /// brightest pixels. The result is less obscured by outliers this way.
-        /// </summary>
-        /// <param name="image">The first image</param>
-        /// <param name="reference">The second image</param>
-        /// <param name="percentage">Percentage of pixels to ignore</param>
-        public static float MSE_OutlierRejection(ImageBase image, ImageBase reference, float percentage = 0.1f) {
-            Debug.Assert(image.Width == reference.Width);
-            Debug.Assert(image.Height == reference.Height);
-            Debug.Assert(image.NumChannels == reference.NumChannels);
-            return SimpleImageIOCore.ComputeMSEOutlierReject(image.DataPointer, image.NumChannels * image.Width,
-                reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
-                image.NumChannels, percentage);
-        }
+    /// <summary>
+    /// Computes the relative mean square error of two images. Ignores a small percentage of the
+    /// brightest pixels. The result is less obscured by outliers this way.
+    /// </summary>
+    /// <param name="image">The first image</param>
+    /// <param name="reference">The second image</param>
+    /// <param name="percentage">Percentage of pixels to ignore</param>
+    public static float MSE_OutlierRejection(ImageBase image, ImageBase reference, float percentage = 0.1f) {
+        Debug.Assert(image.Width == reference.Width);
+        Debug.Assert(image.Height == reference.Height);
+        Debug.Assert(image.NumChannels == reference.NumChannels);
+        return SimpleImageIOCore.ComputeMSEOutlierReject(image.DataPointer, image.NumChannels * image.Width,
+            reference.DataPointer, image.NumChannels * reference.Width, image.Width, image.Height,
+            image.NumChannels, percentage);
     }
 }
