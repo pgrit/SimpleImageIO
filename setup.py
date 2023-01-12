19c74263b4cec36db32b64cb422075578bced2be
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2023-01-12 14:52:17 +0100

    v1.2.0 (no 'default' layer)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='simpleimageio',
-    version='1.1.0',
+    version='1.2.0',
     author='Pascal Grittmann',
     url='https://github.com/pgrit/SimpleImageIO',
 
