632560c5abe208ead3b26dd542df8c6ea6279938
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2023-02-21 17:58:05 +0100

    format

--- SimpleImageIO/Denoiser.cs
+++ SimpleImageIO/Denoiser.cs
@@ -1,75 +1,73 @@
-using System;
+namespace SimpleImageIO;
 
-namespace SimpleImageIO {
+/// <summary>
+/// Denoiser for images rendered with Monte Carlo. Powered by Intel Open Image Denoise.
+/// </summary>
+public class Denoiser : IDisposable {
+    readonly IntPtr device;
+    private bool disposed;
+
     /// <summary>
-    /// Denoiser for images rendered with Monte Carlo. Powered by Intel Open Image Denoise.
+    /// Initializes the denoiser with the default device
     /// </summary>
-    public class Denoiser : IDisposable {
-        readonly IntPtr device;
-        private bool disposed;
+    public Denoiser() {
+        device = OpenImageDenoise.oidnNewDevice(OIDNDeviceType.OIDN_DEVICE_TYPE_DEFAULT);
+        OpenImageDenoise.oidnCommitDevice(device);
+    }
 
-        /// <summary>
-        /// Initializes the denoiser with the default device
-        /// </summary>
-        public Denoiser() {
-            device = OpenImageDenoise.oidnNewDevice(OIDNDeviceType.OIDN_DEVICE_TYPE_DEFAULT);
-            OpenImageDenoise.oidnCommitDevice(device);
-        }
+    /// <summary>
+    /// Releases the OIDN device
+    /// </summary>
+    ~Denoiser() {
+        if (!disposed) OpenImageDenoise.oidnReleaseDevice(device);
+    }
 
-        /// <summary>
-        /// Releases the OIDN device
-        /// </summary>
-        ~Denoiser() {
-            if (!disposed) OpenImageDenoise.oidnReleaseDevice(device);
-        }
+    static void SetFilterImage(IntPtr filter, Image image, string name) {
+        OpenImageDenoise.oidnSetSharedFilterImage(filter, name, image.DataPointer,
+            (OIDNFormat)image.NumChannels, (nuint)image.Width, (nuint)image.Height, 0, 0, 0);
+    }
 
-        static void SetFilterImage(IntPtr filter, Image image, string name) {
-            OpenImageDenoise.oidnSetSharedFilterImage(filter, name, image.DataPointer,
-                (OIDNFormat)image.NumChannels, (nuint)image.Width, (nuint)image.Height, 0, 0, 0);
-        }
+    /// <summary>
+    /// Denoises the given "color" image with optional auxiliary feature images
+    /// </summary>
+    /// <param name="color">The image to denoise (should be rendered with Monte Carlo)</param>
+    /// <param name="albedo">
+    ///     Reflectance color at the primary hit point, to separate texture from noise
+    /// </param>
+    /// <param name="normal">Surface normal at the primary hit point</param>
+    /// <returns>Denoised copy of the image</returns>
+    public RgbImage Denoise(RgbImage color, RgbImage albedo = null, RgbImage normal = null) {
+        IntPtr filter = OpenImageDenoise.oidnNewFilter(device, "RT");
 
-        /// <summary>
-        /// Denoises the given "color" image with optional auxiliary feature images
-        /// </summary>
-        /// <param name="color">The image to denoise (should be rendered with Monte Carlo)</param>
-        /// <param name="albedo">
-        ///     Reflectance color at the primary hit point, to separate texture from noise
-        /// </param>
-        /// <param name="normal">Surface normal at the primary hit point</param>
-        /// <returns>Denoised copy of the image</returns>
-        public RgbImage Denoise(RgbImage color, RgbImage albedo = null, RgbImage normal = null) {
-            IntPtr filter = OpenImageDenoise.oidnNewFilter(device, "RT");
+        SetFilterImage(filter, color, "color");
+        if (albedo != null)
+            SetFilterImage(filter, albedo, "albedo");
+        if (normal != null)
+            SetFilterImage(filter, normal, "normal");
 
-            SetFilterImage(filter, color, "color");
-            if (albedo != null)
-                SetFilterImage(filter, albedo, "albedo");
-            if (normal != null)
-                SetFilterImage(filter, normal, "normal");
+        RgbImage output = new(color.Width, color.Height);
+        SetFilterImage(filter, output, "output");
 
-            RgbImage output = new(color.Width, color.Height);
-            SetFilterImage(filter, output, "output");
+        OpenImageDenoise.oidnSetFilter1b(filter, "hdr", true);
+        OpenImageDenoise.oidnCommitFilter(filter);
 
-            OpenImageDenoise.oidnSetFilter1b(filter, "hdr", true);
-            OpenImageDenoise.oidnCommitFilter(filter);
+        OpenImageDenoise.oidnExecuteFilter(filter);
 
-            OpenImageDenoise.oidnExecuteFilter(filter);
+        OpenImageDenoise.oidnReleaseFilter(filter);
 
-            OpenImageDenoise.oidnReleaseFilter(filter);
+        OIDNError errorCode = OpenImageDenoise.oidnGetDeviceError(device, out string errorMessage);
+        if (errorCode != OIDNError.OIDN_ERROR_NONE)
+            throw new Exception($"OpenImageDenoise failed ({errorCode}): {errorMessage}");
 
-            OIDNError errorCode = OpenImageDenoise.oidnGetDeviceError(device, out string errorMessage);
-            if (errorCode != OIDNError.OIDN_ERROR_NONE)
-                throw new Exception($"OpenImageDenoise failed ({errorCode}): {errorMessage}");
+        return output;
+    }
 
-            return output;
-        }
-
-        /// <summary>
-        /// Releases the OIDN device
-        /// </summary>
-        public void Dispose() {
-            if (!disposed) OpenImageDenoise.oidnReleaseDevice(device);
-            disposed = true;
-            GC.SuppressFinalize(this);
-        }
+    /// <summary>
+    /// Releases the OIDN device
+    /// </summary>
+    public void Dispose() {
+        if (!disposed) OpenImageDenoise.oidnReleaseDevice(device);
+        disposed = true;
+        GC.SuppressFinalize(this);
     }
 }
--- SimpleImageIO/OpenImageDenoise.cs
+++ SimpleImageIO/OpenImageDenoise.cs
@@ -1,92 +1,74 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-namespace SimpleImageIO {
-    internal enum OIDNDeviceType {
-        OIDN_DEVICE_TYPE_DEFAULT = 0, // select device automatically
+namespace SimpleImageIO;
 
-        OIDN_DEVICE_TYPE_CPU = 1, // CPU device
-    }
+internal enum OIDNDeviceType {
+    OIDN_DEVICE_TYPE_DEFAULT = 0, // select device automatically
 
-    internal enum OIDNFormat {
-        OIDN_FORMAT_UNDEFINED = 0,
+    OIDN_DEVICE_TYPE_CPU = 1, // CPU device
+}
 
-        // 32-bit single-precision floating point scalar and vector formats
-        OIDN_FORMAT_FLOAT = 1,
-        OIDN_FORMAT_FLOAT2 = 2,
-        OIDN_FORMAT_FLOAT3 = 3,
-        OIDN_FORMAT_FLOAT4 = 4,
-    }
+internal enum OIDNFormat {
+    OIDN_FORMAT_UNDEFINED = 0,
 
-    internal enum OIDNError {
-        OIDN_ERROR_NONE = 0, // no error occurred
-        OIDN_ERROR_UNKNOWN = 1, // an unknown error occurred
-        OIDN_ERROR_INVALID_ARGUMENT = 2, // an invalid argument was specified
-        OIDN_ERROR_INVALID_OPERATION = 3, // the operation is not allowed
-        OIDN_ERROR_OUT_OF_MEMORY = 4, // not enough memory to execute the operation
-        OIDN_ERROR_UNSUPPORTED_HARDWARE = 5, // the hardware (e.g. CPU) is not supported
-        OIDN_ERROR_CANCELLED = 6, // the operation was cancelled by the user
-    }
+    // 32-bit single-precision floating point scalar and vector formats
+    OIDN_FORMAT_FLOAT = 1,
+    OIDN_FORMAT_FLOAT2 = 2,
+    OIDN_FORMAT_FLOAT3 = 3,
+    OIDN_FORMAT_FLOAT4 = 4,
+}
 
-    internal static class OpenImageDenoise {
-        const string LibName = "OpenImageDenoise";
+internal enum OIDNError {
+    OIDN_ERROR_NONE = 0, // no error occurred
+    OIDN_ERROR_UNKNOWN = 1, // an unknown error occurred
+    OIDN_ERROR_INVALID_ARGUMENT = 2, // an invalid argument was specified
+    OIDN_ERROR_INVALID_OPERATION = 3, // the operation is not allowed
+    OIDN_ERROR_OUT_OF_MEMORY = 4, // not enough memory to execute the operation
+    OIDN_ERROR_UNSUPPORTED_HARDWARE = 5, // the hardware (e.g. CPU) is not supported
+    OIDN_ERROR_CANCELLED = 6, // the operation was cancelled by the user
+}
 
-        // static OpenImageDenoise() {
-        //     NativeLibrary.SetDllImportResolver(typeof(OpenImageDenoise).Assembly, ImportResolver);
-        // }
+internal static class OpenImageDenoise {
+    const string LibName = "OpenImageDenoise";
 
-        // private static IntPtr ImportResolver(string libraryName, Assembly assembly,
-        //                                      DllImportSearchPath? dllImportSearchPath) {
-        //     string mappedName = libraryName;
+    #region Device
 
-        //     // Linking on OS X only works correctly if the file contains the version number.
-        //     if (libraryName == LibName && RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-        //         mappedName = "libOpenImageDenoise.1.4.3.dylib";
-        //     }
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr oidnNewDevice(OIDNDeviceType type);
 
-        //     return NativeLibrary.Load(mappedName, assembly, dllImportSearchPath);
-        // }
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnReleaseDevice(IntPtr device);
 
-        #region Device
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnCommitDevice(IntPtr device);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr oidnNewDevice(OIDNDeviceType type);
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern OIDNError oidnGetDeviceError(IntPtr device, [Out] out string outMessage);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnReleaseDevice(IntPtr device);
+    #endregion Device
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnCommitDevice(IntPtr device);
+    #region Filters
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern OIDNError oidnGetDeviceError(IntPtr device, [Out] out string outMessage);
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr oidnNewFilter(IntPtr device, string type);
 
-        #endregion Device
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnSetSharedFilterImage(IntPtr filter, string name,
+        IntPtr ptr, OIDNFormat format, nuint width, nuint height, nuint byteOffset,
+        nuint bytePixelStride, nuint byteRowStride);
 
-        #region Filters
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnSetFilter1b(IntPtr filter, string name, bool value);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr oidnNewFilter(IntPtr device, string type);
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnCommitFilter(IntPtr filter);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnSetSharedFilterImage(IntPtr filter, string name,
-            IntPtr ptr, OIDNFormat format, nuint width, nuint height, nuint byteOffset,
-            nuint bytePixelStride, nuint byteRowStride);
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnExecuteFilter(IntPtr filter);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnSetFilter1b(IntPtr filter, string name, bool value);
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void oidnReleaseFilter(IntPtr filter);
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnCommitFilter(IntPtr filter);
+    #endregion Filters
 
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnExecuteFilter(IntPtr filter);
-
-        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void oidnReleaseFilter(IntPtr filter);
-
-        #endregion Filters
-
-    }
 }
