1d18eb9d984e9cfc8d4dd8e9bd2bcbe593802f35
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2023-05-11 16:47:12 +0200

    test new ci

--- make.ps1
+++ make.ps1
@@ -1,78 +1,119 @@
-$renderLibVersion = "0.1.1"
-if (-not(Test-Path -Path "prebuilt" -PathType Container))
+param(
+    [string] $renderLibVersion = "0.1.1",
+    [string] $prebuiltFrontend = $null,
+    [boolean] $clean = $false,
+    [boolean] $skipRuntimes = $false
+)
+
+if (-not $skipRuntimes)
 {
-    echo "Downloading precompiled binaries for OIDN..."
-    Invoke-WebRequest -Uri "https://github.com/pgrit/RenderLibs/releases/download/v$renderLibVersion/RenderLibs-v$renderLibVersion.zip" -OutFile "prebuilt.zip"
-    Expand-Archive "prebuilt.zip" -DestinationPath ./prebuilt
-    rm prebuilt.zip
+
+    if (-not(Test-Path -Path "prebuilt" -PathType Container) -or $clean)
+    {
+        echo "Downloading precompiled binaries for OIDN..."
+        Invoke-WebRequest -Uri "https://github.com/pgrit/RenderLibs/releases/download/v$renderLibVersion/RenderLibs-v$renderLibVersion.zip" -OutFile "prebuilt.zip"
+        Expand-Archive "prebuilt.zip" -DestinationPath ./prebuilt
+        rm prebuilt.zip
+    }
+
+    # Make sure the required directories exist (but silently)
+    mkdir -Force runtimes > $null
+    mkdir -Force runtimes/linux-x64 > $null
+    mkdir -Force runtimes/linux-x64/native > $null
+    mkdir -Force runtimes/win-x64 > $null
+    mkdir -Force runtimes/win-x64/native > $null
+    mkdir -Force runtimes/osx-x64 > $null
+    mkdir -Force runtimes/osx-x64/native > $null
+    mkdir -Force runtimes/osx-arm64 > $null
+    mkdir -Force runtimes/osx-arm64/native > $null
+    mkdir -Force build > $null
+
+    echo "Copying files for OIDN..."
+
+    cp prebuilt/linux/lib/libtbb.so.12.8 runtimes/linux-x64/native/libtbb.so.12
+    cp prebuilt/linux/lib/libOpenImageDenoise.so.1.4.3 runtimes/linux-x64/native/libOpenImageDenoise.so
+
+    cp prebuilt/win/bin/tbb12.dll runtimes/win-x64/native/
+    cp prebuilt/win/bin/OpenImageDenoise.dll runtimes/win-x64/native/OpenImageDenoise.dll
+
+    cp prebuilt/osx/lib/libtbb.12.8.dylib runtimes/osx-x64/native/libtbb.12.dylib
+    cp prebuilt/osx/lib/libOpenImageDenoise.1.4.3.dylib runtimes/osx-x64/native/libOpenImageDenoise.dylib
+
+    cp prebuilt/osx/lib/libtbb.12.8.dylib runtimes/osx-arm64/native/libtbb.12.dylib
+    cp prebuilt/osx-arm64/lib/libOpenImageDenoise.1.4.3.dylib runtimes/osx-arm64/native/libOpenImageDenoise.dylib
+
+    echo "Compiling SimpleImageIOCore..."
+
+    cd build
+
+    if ([environment]::OSVersion::IsMacOS())
+    {
+        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64" ..
+        if (-not $?) { throw "CMake configure failed" }
+        cmake --build . --config Release
+        if (-not $?) { throw "Build failed" }
+
+        # Empty the build folder first to avoid cache issues
+        rm -rf *
+
+        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64" ..
+        if (-not $?) { throw "CMake configure failed" }
+        cmake --build . --config Release
+        if (-not $?) { throw "Build failed" }
+    }
+    else
+    {
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        if (-not $?) { throw "CMake configure failed" }
+
+        cmake --build . --config Release
+        if (-not $?) { throw "Build failed" }
+    }
+
+    cd ..
 }
 
-echo "Copying files for OIDN..."
-mkdir runtimes
-
-mkdir runtimes/linux-x64
-mkdir runtimes/linux-x64/native
-cp prebuilt/linux/lib/libtbb.so.12.8 runtimes/linux-x64/native/libtbb.so.12
-cp prebuilt/linux/lib/libOpenImageDenoise.so.1.4.3 runtimes/linux-x64/native/libOpenImageDenoise.so
-
-mkdir runtimes/win-x64
-mkdir runtimes/win-x64/native
-cp prebuilt/win/bin/tbb12.dll runtimes/win-x64/native/
-cp prebuilt/win/bin/OpenImageDenoise.dll runtimes/win-x64/native/OpenImageDenoise.dll
-
-mkdir runtimes/osx-x64
-mkdir runtimes/osx-x64/native
-cp prebuilt/osx/lib/libtbb.12.8.dylib runtimes/osx-x64/native/libtbb.12.dylib
-cp prebuilt/osx/lib/libOpenImageDenoise.1.4.3.dylib runtimes/osx-x64/native/libOpenImageDenoise.dylib
-
-mkdir runtimes/osx-arm64
-mkdir runtimes/osx-arm64/native
-cp prebuilt/osx/lib/libtbb.12.8.dylib runtimes/osx-arm64/native/libtbb.12.dylib
-cp prebuilt/osx-arm64/lib/libOpenImageDenoise.1.4.3.dylib runtimes/osx-arm64/native/libOpenImageDenoise.dylib
-
-echo "Compiling SimpleImageIOCore..."
-
-mkdir build
-cd build
-
-if ([environment]::OSVersion::IsMacOS())
+if ($prebuiltFrontend)
 {
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64" ..
-    if (-not $?) { throw "CMake configure failed" }
-    cmake --build . --config Release
-    if (-not $?) { throw "Build failed" }
-
-    # Empty the build folder first to avoid cache issues
-    rm -rf *
-
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64" ..
-    if (-not $?) { throw "CMake configure failed" }
-    cmake --build . --config Release
-    if (-not $?) { throw "Build failed" }
+    cp $prebuiltFrontend PyWrapper/simpleimageio/flipbook.js
+    mkdir -Force ./FlipViewer/dist
+    cp $prebuiltFrontend ./FlipViewer/dist/flipbook.js
 }
 else
 {
-    cmake -DCMAKE_BUILD_TYPE=Release ..
-    if (-not $?) { throw "CMake configure failed" }
+    echo "Bundling react viewer..."
 
-    cmake --build . --config Release
-    if (-not $?) { throw "Build failed" }
+    cd ./FlipViewer
+    npm install
+    npm run build
+    if (-not $?) { throw "node / webpack build failed - core tools operational, but flip books will not function" }
+
+    cd ..
+
+    echo "Copying .js to python package..."
+    cp ./FlipViewer/dist/flipbook.js PyWrapper/simpleimageio/flipbook.js
 }
 
+echo "Building the python wrapper"
+
+python -m build
+if (-not $?) { throw "Build failed" }
+
+$latestWhl = Get-ChildItem -Path "./dist/*.whl" | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+echo "Installing $latestWhl"
+python -m pip install --user $latestWhl.FullName
+python -m pip install --user --force-reinstall --no-deps $latestWhl.FullName
+if (-not $?) { throw "Install failed" }
+
+echo "Testing python lib"
+cd PyTest
+python -m unittest
+if (-not $?) { throw "Test failed" }
 cd ..
 
-echo "Bundling react viewer..."
+echo "Build and test the C# lib"
 
-cd ./FlipViewer
-npm install
-npm run build
-if (-not $?) { throw "node / webpack build failed - core tools operational, but flip books will not function" }
-
-cd ..
-
-echo "Copying .js to python package..."
-cp ./FlipViewer/dist/flipbook.js PyWrapper/simpleimageio/flipbook.js
-
-# Test the C# wrapper
 dotnet build
+if (-not $?) { throw "Build failed" }
 dotnet test
+if (-not $?) { throw "Test failed" }
